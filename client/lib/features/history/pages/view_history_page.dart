5c1423f7e29eb006da04e2829f6941d098a45615
Zendrex <162876828+zendrexx@users.noreply.github.com>
2025-10-01 14:04:35 +0800

    view history detail page done

--- client/lib/features/history/pages/view_history_page.dart
+++ client/lib/features/history/pages/view_history_page.dart
@@ -1,3 +1,5 @@
+import 'package:client/features/history/widgets/workout_details_widget.dart';
+import 'package:client/features/history/widgets/workout_row_widget.dart';
 import 'package:flutter/material.dart';
 
 class ViewHistoryPage extends StatelessWidget {
@@ -24,7 +26,74 @@ class ViewHistoryPage extends StatelessWidget {
         scrolledUnderElevation: 6,
         surfaceTintColor: Colors.transparent,
       ),
-      body: SingleChildScrollView(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "PUSH DAY",
+                style: TextStyle(
+                  fontSize: 22,
+                  height: 1,
+                  color: Color(0xffE2725B),
+                ),
+              ),
+              Text(
+                "12 mins ago",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  height: 1.5,
+                  color: Color(0xff2F4F4F),
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Time",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          height: 1,
+                          color: Color(0xff89898A),
+                        ),
+                      ),
+                      Text("1h 10min"),
+                    ],
+                  ),
+                  SizedBox(width: 40),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Volume",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          height: 1,
+                          color: Color(0xff89898A),
+                        ),
+                      ),
+                      Text("10,002.1 lbs"),
+                    ],
+                  ),
+                ],
+              ),
+              Divider(),
+              WorkoutDetailsWidget(),
+              WorkoutDetailsWidget(),
+              WorkoutDetailsWidget(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- client/lib/features/history/widgets/workout_details_widget.dart
+++ client/lib/features/history/widgets/workout_details_widget.dart
@@ -1,44 +1,40 @@
-import 'package:client/features/history/widgets/workout_row_widget.dart';
 import 'package:client/features/home/widgets/workout_set_widget.dart';
 import 'package:flutter/material.dart';
 
-class SessionWorkoutWidget extends StatelessWidget {
-  const SessionWorkoutWidget({super.key});
+class WorkoutDetailsWidget extends StatelessWidget {
+  const WorkoutDetailsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 16.0),
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const CircleAvatar(
-                radius: 30,
+              CircleAvatar(
+                minRadius: 30,
                 backgroundImage: AssetImage("assets/images/benchpress.png"),
               ),
-              SizedBox(width: 5),
+              SizedBox(width: 16),
               Text(
                 "Incline Bench Press (Barbell)",
                 style: TextStyle(
-                  color: Color(0xffE2725B),
+                  color: Color(0xffD3C061),
                   fontSize: 16,
-                  wordSpacing: .5,
+                  fontWeight: FontWeight.w500,
                 ),
-              ),
-              Spacer(),
-              GestureDetector(
-                child: Icon(Icons.more_vert_outlined, color: Colors.white),
               ),
             ],
           ),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Add session notes here",
-              border: InputBorder.none,
-              hintStyle: TextStyle(fontSize: 14),
-            ),
+          SizedBox(height: 10),
+          Text(
+            "Sobrang Bigat",
+            style: TextStyle(color: Color(0xff4E4E50), fontSize: 14),
           ),
+          SizedBox(height: 15),
           Row(
             children: [
               Expanded(
@@ -64,8 +60,8 @@ class SessionWorkoutWidget extends StatelessWidget {
               Spacer(),
             ],
           ),
-          SizedBox(height: 5),
           WorkoutSetWidget(),
+
           WorkoutSetWidget(),
           WorkoutSetWidget(),
         ],
