3b6adecac448c0dfe12c0de9115b2ec981eaabd3
Zendrex <162876828+zendrexx@users.noreply.github.com>
2025-12-26 22:01:58 +0800

    .

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
+  //providerscope only hindi na init ng dbase
   runApp(ProviderScope(child: MyApp()));
 }
 
