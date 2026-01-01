99b510442daaf02ea2764fea7c9a41ae4f77921f
Zendrex <162876828+zendrexx@users.noreply.github.com>
2026-01-01 19:13:41 +0800

    .

--- client/lib/data/abstract/planned_session_abstract.dart
+++ client/lib/data/abstract/planned_session_abstract.dart
@@ -2,4 +2,6 @@ import 'package:client/data/models/planned_session.dart';
 
 abstract class PlannedSessionAbstract {
   Future<List<PlannedSession>> getAllPlannedSession();
+  Future<void> addSession(PlannedSession plannedSession);
+  //just to push jan 1
 }
