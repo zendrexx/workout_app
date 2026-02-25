ed247fb96e9c54fcbe93d6b86f5952b1f697e8ce
Zendrex <162876828+zendrexx@users.noreply.github.com>
2026-02-25 12:00:00 +0800

    commit

--- client/lib/features/workout_logging/presentation/widgets/log_session_workout_widget.dart
+++ client/lib/features/workout_logging/presentation/widgets/log_session_workout_widget.dart
@@ -256,7 +256,7 @@ class _LogSessionWorkoutWidgetState
                 repRange: widget.plannedSets[setIndex].estRep,
                 setId: widget.plannedSets[setIndex].setId,
                 key: ValueKey(sets[setIndex].setId),
-              ); //day 2 no code
+              ); //day 2 no codeasdas
             },
           ),
           SizedBox(height: 10),
